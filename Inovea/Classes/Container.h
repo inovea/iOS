04297152439006cd5c14734452ed997601fbdb00
Louis Sebastien <inovea.esgi@gmail.com>
2015-03-10 22:31:56 +0100

    Container class implemented

---
+++ Inovea/Classes/Container.h
@@ -0,0 +1,49 @@
+//
+//  Container.h
+//  Inovea
+//
+//  Created by Sébastien LOUIS on 10/03/2015.
+//  Copyright (c) 2015 Inovea. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Container : NSObject
+{
+@private
+    long idd_;
+    NSString* lat_;
+    NSString* lng_;
+    NSString* address_;
+    Boolean state_;
+    NSArray* alerts_;
+}
+
+@property (nonatomic,assign)long idd;
+@property (nonatomic, strong) NSString* lat;
+@property (nonatomic, strong) NSString* lng;
+@property (nonatomic, strong) NSString* address;
+@property (nonatomic,assign)Boolean state;
+@property (nonatomic, strong) NSArray* alerts;
+
+-(id)initContainerWithId :(long)idd andLat:(NSString*)lat andLng: (NSString*)lng andAddress: (NSString*)address andState: (Boolean)state andAlerts:(NSArray*)alerts;
+
+-(long) getId;
+-(void) setIdd:(long)idd;
+
+-(NSString*) getLat;
+-(void) setLat:(NSString*)lat;
+
+-(NSString*) getLng;
+-(void) setLng:(NSString *)lng;
+
+-(NSString*) getAddress;
+-(void) setAddress:(NSString*)address;
+
+-(Boolean) isState;
+-(void) setState:(Boolean)state;
+
+-(NSArray*) getAlerts;
+-(void) setAlerts:(NSArray *)alerts;
+
+@end
---
+++ Inovea/Classes/Container.m
@@ -0,0 +1,83 @@
+//
+//  Container.m
+//  Inovea
+//
+//  Created by Sébastien LOUIS on 10/03/2015.
+//  Copyright (c) 2015 Inovea. All rights reserved.
+//
+
+#import "Container.h"
+
+@implementation Container
+
+@synthesize idd = idd_;
+@synthesize lat = lat_;
+@synthesize lng = lng_;
+@synthesize address = address_;
+@synthesize state = state_;
+@synthesize alerts = alerts_;
+
+
+-(id)initContainerWithId :(long)idd andLat:(NSString*)lat andLng: (NSString*)lng andAddress: (NSString*)address andState: (Boolean)state andAlerts:(NSArray*)alerts{
+    self = [super init];
+    self.idd = idd;
+    self.lat = lat;
+    self.lng = lng;
+    self.address = address;
+    self.state = state;
+    self.alerts = alerts;
+    return self;
+}
+
+
+-(long) getId{
+    return self.idd;
+}
+
+-(void) setIdd:(long)idd{
+    self.idd = idd;
+}
+
+-(NSString*) getLat{
+    return self.lat;
+}
+
+-(void) setLat:(NSString*)lat{
+    self.lat = lat;
+}
+
+-(NSString*) getLng{
+    return self.lng;
+}
+
+-(void) setLng:(NSString *)lng{
+    self.lng = lng;
+}
+
+-(NSString*) getAddress{
+    return self.address;
+}
+
+-(void) setAddress:(NSString*)address{
+    self.address = address;
+}
+
+-(Boolean) isState{
+    return self.state;
+}
+
+-(void) setState:(Boolean)state{
+    self.state = state;
+}
+
+-(NSArray*) getAlerts{
+    return self.alerts;
+}
+
+-(void) setAlerts:(NSArray *)alerts{
+    self.alerts = alerts;
+}
+
+@end
+
+
